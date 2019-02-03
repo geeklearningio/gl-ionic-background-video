a7b170e62cc855b5448b5a5ef1245ffcb4745d37
mvidailhet <michel.vidailhet@gmail.com>
2019-02-03 16:13:18 +0100

    component is ready

---
+++ src/components/gl-background-video/gl-background-video.tsx
@@ -0,0 +1,24 @@
+import { Component, Prop, Element } from '@stencil/core';
+
+@Component({
+  tag: 'gl-background-video',
+  styleUrl: 'gl-background-video.css'
+})
+export class MyComponent {
+  @Prop() src: string;
+  @Prop() poster: string;
+  @Prop() muted: boolean = true;
+
+  @Element() private videoElement: HTMLVideoElement;
+
+  componentDidLoad() {
+    // sometimes, as the component is loaded dynamically, the video starts with sound, even if muted property is set to true
+    if (this.muted) {
+      this.videoElement.muted = true;
+    }
+  }
+
+  render() {
+    return <video autoplay loop playsinline preload="auto" muted={this.muted} src={this.src} poster={this.poster}></video>;
+  }
+}
---
+++ src/components/gl-background-video/readme.md
@@ -0,0 +1,19 @@
+# gl-background-video
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type      | Default     |
+| -------- | --------- | ----------- | --------- | ----------- |
+| `muted`  | `muted`   |             | `boolean` | `true`      |
+| `poster` | `poster`  |             | `string`  | `undefined` |
+| `src`    | `src`     |             | `string`  | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
